21d321b09e6039232193895a502a72e6bc72df00
Sergio Henrique Beppu <sergio.beppu@Ginhu-PC>
2024-03-05 11:12:31 -0300

    feat: codigo desenvolvido durante as aulas e codigo da tarefa da seção 2

---
+++ section2task.py
@@ -0,0 +1,36 @@
+# Tarefa um atribuir a variáveis o nome e idade do usuário
+# nome = input('Por favor digite o seu nome: ')
+# idade = int(input('Agora por favor digite sua idade: '))
+
+nome_usuario = input('Por favor digite o seu nome: ')
+idade_usuario = int(input('Agora por favor digite sua idade: '))
+
+
+def imprimir_nome_idade():
+    """Tarefa dois criar uma função que imprime na tela o nome e a idade
+     em uma string apenas"""
+    # print('nome: ', nome, '- idade: ', idade)
+    print('nome: ', nome_usuario, '- idade: ', idade_usuario)
+
+
+imprimir_nome_idade()
+
+
+def imprimir_argumentos(argumento1='Sem argumento', argumento2='Pois é'):
+    print(argumento1, ' - concluindo:', argumento2)
+
+
+argumento1 = input('Digite aqui seu primeiro argumento: ')
+argumento2 = input('Digite aqui seu segundo argumento: ')
+imprimir_argumentos()
+argumento1 = input('Digite aqui seu primeiro argumento: ')
+argumento2 = input('Digite aqui seu segundo argumento: ')
+imprimir_argumentos(argumento1, argumento2)
+
+
+def calculo_de_decadas():
+    # return idade//10
+    return idade_usuario//10
+
+
+print(calculo_de_decadas())
