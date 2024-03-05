9557fe36cb232728f31470fda58966e7959de17a
Sergio Henrique Beppu <sergio.beppu@Ginhu-PC>
2024-03-05 15:15:23 -0300

    feat: tarefas finalizadas

--- section3/section3.py
+++ section3/section3.py
@@ -1,2 +1,18 @@
 # Tarefa 1
-lista_nomes = ['guga', 'lucas', 'gabi', 'ginhu', 'sergio', 'nice']
+lista_nomes = ['guga', 'lucas', 'gabi', 'giNhu', 'sergio', 'nice']
+
+for nome in lista_nomes:
+    """Tarefa 1"""
+    # print(len(nome))
+
+    """Tarefa 2 e 3"""
+    if len(nome) > 5 and ('n' in nome or 'N' in nome):
+        print(len(nome))
+
+# Tarefa 4
+condicional = True
+while condicional:
+    lista_nomes.pop()
+    if len(lista_nomes) == 0:
+        condicional = False
+print('finalizado')
